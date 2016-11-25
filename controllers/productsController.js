f3fadabc450af76b0f7a8fd371423050db5472f4
Juan Carlos Ruiz <JuanCrg90@gmail.com>
2016-11-25 09:34:14 -0600

    Add API Controller

---
+++ controllers/productsController.js
@@ -0,0 +1,21 @@
+
+
+exports.findAllProducts = function(req, res) {
+	res.send('Find All');
+};
+
+exports.findById = function(req, res) {
+	res.send('findById');
+};
+
+exports.addProduct = function(req, res) {
+	res.send('Add Product');
+};
+
+exports.updateProduct = function(req, res) {
+	res.send('Update Product');
+};
+
+exports.deleteProduct = function(req, res) {
+	res.send('Delete Product');
+};
--- index.js
+++ index.js
@@ -2,17 +2,29 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverrride = require('method-override');
 var mongoose = require('mongoose');
+var productsController = require('./controllers/productsController.js')
 
 var app = express();
+var products = express.Router();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(methodOverrride());
+app.use('/api', products);
 
 app.get('/', function(req, res) {
 	res.send('Hello World');
 });
 
+products.route('/products')
+.get(productsController.findAllProducts)
+.post(productsController.addProduct);
+
+products.route('/products/:id')
+.get(productsController.findById)
+.put(productsController.updateProduct)
+.delete(productsController.deleteProduct);
+
 var server = app.listen(3000, function() {
 	console.log('Express server listening in port ' + server.address().port);
-})
+});
